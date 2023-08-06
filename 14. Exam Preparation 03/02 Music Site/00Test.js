f5dea393fd47392bdd6e131bef9ec5773d2a45a0
LachezarIvanov-git <118828142+LachezarIvanov-git@users.noreply.github.com>
2023-08-07 01:54:30 +0300

    a  little code added just scravhes

---
+++ 14. Exam Preparation 03/02 Music Site/00Test.js
@@ -0,0 +1,32 @@
+
+const walk = (name)=>{
+
+    
+    return `${name} is walking`
+    
+}
+
+const swimming = (name)=>{
+
+   
+    return `${name} is swimming`
+    
+    
+}
+
+function doIt(action){
+    
+    console.log(`${action}`);
+}
+
+console.log(walk('Jeniffer'))
+
+walk('pesho')
+
+
+
+
+
+
+
+
